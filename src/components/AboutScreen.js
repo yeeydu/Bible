a9f010ca07b4c8c4b91a0aa93fd5ac347555ca87
yeeysonduarte <portas3@hotmail.com>
2023-12-04 15:32:32 +0000

    add link color

--- src/components/AboutScreen.js
+++ src/components/AboutScreen.js
@@ -1,5 +1,5 @@
 import { View, Text, StyleSheet, Linking } from 'react-native'
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import ItemSeparator from './ItemSeparator';
 import ThemeContext from '../theme/themeContext';
 
@@ -7,29 +7,30 @@ export default function AboutScreen() {
 
   const theme = useContext(ThemeContext);
 
+
   return (
-    <View style={[styles.container, {color:theme.color}]}>
-      <Text style={[styles.text,{color:theme.color}]}>The King James Version (KJV), also the King James Bible (KJB) and the Authorized Version (AV) is an Early Modern English translation of the Christian Bible for the Church of England, which was commissioned in 1604 and published in 1611, by sponsorship of King James VI and I.
+    <View style={[styles.container, { color: theme.color }]}>
+      <Text style={[styles.text, { color: theme.color }]}>The King James Version (KJV), also the King James Bible (KJB) and the Authorized Version (AV) is an Early Modern English translation of the Christian Bible for the Church of England, which was commissioned in 1604 and published in 1611, by sponsorship of King James VI and I.
       </Text>
-      <Text style={[styles.text,{color:theme.color}] }>
+      <Text style={[styles.text, { color: theme.color }]}>
         The King James Bible has long been celebrated as one of the most significant texts of all time, not only for its accessible portrayal of the Christian religion, but also for its ability to spread the English language worldwide to become the dominant global language (in both a commercial and cultural sense) that it is today.
       </Text>
-      <Text style={[styles.subTitle, {color:theme.color}]}>
+      <Text style={[styles.subTitle, { color: theme.color }]}>
         Copyright status
       </Text>
-      <Text style={[styles.text, {color:theme.color}]}>
+      <Text style={[styles.text, { color: theme.color }]}>
 
         The Authorized Version is in the public domain in most of the world. In the United Kingdom, the right to print, publish and distribute it is a royal prerogative and the Crown licenses publishers to reproduce it under letters patent.
       </Text>
       <ItemSeparator />
-      <Text style={[styles.subColaborate, {color:theme.color}]}>
+      <Text style={[styles.subColaborate, { color: theme.color }]}>
         Colaborate
       </Text>
-      <Text style={[styles.subColaborate, styles.link,  {color:theme.color}]} onPress={() => Linking.openURL('https://buymeacoffee.com/extramedia19')}>
-       👉 By a Coffee
+      <Text style={[styles.subColaborate, styles.link,]} onPress={() => Linking.openURL('https://buymeacoffee.com/extramedia19')}>
+        👉 By a Coffee
       </Text>
-      <Text style={[styles.subColaborate, styles.link, {color:theme.color}]} onPress={() => Linking.openURL('https://buymeacoffee.com/extramedia19')}>
-       👉 DONATE 
+      <Text style={[styles.subColaborate, styles.link,]} onPress={() => Linking.openURL('https://buymeacoffee.com/extramedia19')}>
+        👉 DONATE
       </Text>
       <ItemSeparator />
     </View>
@@ -56,10 +57,12 @@ const styles = StyleSheet.create({
   subColaborate: {
     fontSize: 20,
     padding: 8,
+    color: '#0096FF',
     fontWeight: 'bold',
     textAlign: 'center',
   },
-  link:{
+  link: {
     color: '#0096FF',
-  }
+    textDecorationLine: 'underline',
+  },
 });
